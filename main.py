50e88da55659f0a867581d3f817821a24032d3c9
vishal <vivevich3@gmail.com>
2022-11-25 20:10:56 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+a=int(input("enter"))
+for i in range(0, 10):
+    for j in range(20, 30):
+while (a>=0):
+     if a==i:
+        print("it is an odd range")
+        break
+     if a==j:
+        print("it is an even range")
+        break
+
